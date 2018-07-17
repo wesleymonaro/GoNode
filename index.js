b41b16223a161a893ecbb25abee8c4b208ab7d92
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-17 11:47:27 -0300

    Função Next()

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res, next) => {
+    res.send('Hello World');
+    next(); //chama o próximo middleware/rota
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
