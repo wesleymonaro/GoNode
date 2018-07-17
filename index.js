e2208a01253c9a8ad2cce604fdb63bf5abcd8836
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-17 11:57:27 -0300

    Middlewares

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 
+//middleware global
+// app.use((req, res, next) => {
+//     console.log('Acessou');
+//     next();
+// })
+
+//custom middleware
+const userMiddleware = (req, res, next) => {
+    console.log('Acessou');
+    next();
+}
+
 app.get('/', (req, res, next) => {
     res.send('Hello World');
     next(); //chama o próximo middleware/rota
 })
 
-app.get('/', (req, res) => {
+app.get('/teste', (req, res) => {
     res.send('Hello World');
+})
+
+app.get('/user', userMiddleware, (req, res) => {
+    res.send('Usuário Wesley');
 })
 
 app.listen(3000);
