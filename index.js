ac152c98e453544e02aba38434bb43cb02e128fa
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-17 13:23:43 -0300

    Configurando ESLint

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const bodyParser = require('body-parser');
 const app = express();
 
 nunjucks.configure('views', {
-    autoescape: true,
-    express: app
-})
+  autoescape: true,
+  express: app,
+});
 
 app.set('view engine', 'njk');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,15 +16,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.render('index')
-})
+  res.render('index');
+});
 
 app.post('/auth', (req, res) => {
-    const { username, password } = req.body;
-
-    res.send(`Você logou com o usuário ${username} e senha ${password}`);
-
-})
+  const { username, password } = req.body;
+  res.send(`Você logou com o usuário ${username} e senha ${password}`);
+});
 
 
 app.listen(3000);
