dc770d5ecdaf399423434dd6af383ea913cf8465
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-23 14:55:15 -0300

    Cadastrando novo usuário

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -1,8 +1,25 @@
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
   signin(req, res) {
     return res.render('auth/signin');
   },
   signup(req, res) {
     return res.render('auth/signup');
+  },
+
+  async register(req, res) {
+    const { email } = req.body;
+
+    if (await User.findOne({ where: { email } })) {
+      return res.redirect('back');
+    }
+
+    const password = await bcrypt.hash(req.body.password, 5);
+
+    await User.create({ ...req.body, password });
+
+    return res.redirect('/');
   },
 };
--- app/routes.js
+++ app/routes.js
@@ -7,4 +7,6 @@ const authController = require('./controllers/authController');
 routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
 
+routes.post('/register', authController.register);
+
 module.exports = routes;
