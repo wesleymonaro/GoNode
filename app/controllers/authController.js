d3ca1f3d22f72710e55143dfb34e94c3236cbd0c
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-23 15:50:02 -0300

    Autenticação

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -23,4 +23,26 @@ module.exports = {
     req.flash('success', 'Usuário cadastrado com sucesso');
     return res.redirect('/');
   },
+
+  async authenticate(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      req.flash('error', 'Usuário inexistente');
+      return res.redirect('back');
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+      req.flash('error', 'Senha incorreta');
+      return res.redirect('back');
+    }
+
+    req.session.user = user;
+
+    return req.session.save(() => {
+      res.redirect('app/dashboard');
+    });
+  },
 };
--- app/routes.js
+++ app/routes.js
@@ -14,5 +14,6 @@ routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
 
 routes.post('/register', authController.register);
+routes.post('/authenticate', authController.authenticate);
 
 module.exports = routes;
