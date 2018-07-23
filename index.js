ed34e6d4cdee715d4bbec408021f0c27b64df2f7
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-23 13:31:48 -0300

    Estrutura do Nunjucks

--- index.js
+++ index.js
@@ -5,24 +5,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-nunjucks.configure('views', {
+nunjucks.configure(path.resolve('app', 'views'), {
   autoescape: true,
   express: app,
 });
 
 app.set('view engine', 'njk');
-app.set('views', path.join(__dirname, 'views'));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
 });
-
-app.post('/auth', (req, res) => {
-  const { username, password } = req.body;
-  res.send(`Você logou com o usuário ${username} e senha ${password}`);
-});
-
 
 app.listen(3000);
