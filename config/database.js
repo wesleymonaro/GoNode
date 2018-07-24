748ee0d6515588c5d9d2a71eec90e3a39a4f028d
Wesley Monaro <wesleymonaro@hotmail.com>
2018-07-24 00:06:59 -0300

    first commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 module.exports = {
-  username: 'root',
-  password: null,
+  // username: 'root',
+  // password: null,
+  // database: 'snippetfy',
+  // host: '127.0.0.1',
+  // dialect: 'mysql',
+  username: 'snippetfy',
+  password: 'A74hNaBM8SFY',
   database: 'snippetfy',
-  host: '127.0.0.1',
+  host: 'mysql556.umbler.com',
+  port: 41890,
   dialect: 'mysql',
 };
--- index.js
+++ index.js
@@ -26,4 +26,4 @@ app.use(methodOverride('_method'));
 
 app.use('/', routes);
 
-app.listen(3000);
+app.listen(process.env.PORT);
