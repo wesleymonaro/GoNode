eb5002f9587cc5341f7ab52b97fcf895c589ccbb
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-05 17:40:50 -0300

    Adicionando Sentry

---
+++ Modulo03/config/sentry.js
@@ -0,0 +1,3 @@
+module.exports = {
+  sentryDSN: 'https://7b6f6d3f9b0a418584b36d72154024e8@sentry.io/1256363',
+};
--- Modulo03/index.js
+++ Modulo03/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
-
+const Raven = require('./app/services/sentry');
 
 const dbConfig = require('./config/database');
 
@@ -11,7 +11,10 @@ requireDir(dbConfig.modelsPath);
 
 app.use(bodyParser.json());
 
+app.use(Raven.requestHandler());
+
 app.use('/api', require('./app/routes'));
 
+app.use(Raven.errorHandler());
 
 app.listen(3000);
