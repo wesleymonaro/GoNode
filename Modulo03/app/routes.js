d45a2a210902f14786a68b43da2f3e19bafdd9a7
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-05 17:32:41 -0300

    Middleware de autenticação

--- Modulo03/app/routes.js
+++ Modulo03/app/routes.js
@@ -3,10 +3,18 @@ const requireDir = require('require-dir');
 
 const routes = express.Router();
 
+const authMiddleware = require('./middlewares/auth');
+
 const controllers = requireDir('./controllers');
 
 // Auth
 routes.post('/signup', controllers.authController.signup);
 routes.post('/signin', controllers.authController.signin);
+
+
+// WITH AUTH
+routes.use(authMiddleware);
+
+routes.get('/tweets', (req, res) => res.send('OK'));
 
 module.exports = routes;
