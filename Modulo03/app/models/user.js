bf287779c966f08d668eec926850b3fdf7d3f54e
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-05 16:43:08 -0300

    Adicionando body-parser e Insomnia

--- Modulo03/app/models/user.js
+++ Modulo03/app/models/user.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  emial: {
+  email: {
     type: String,
     required: true,
     unique: true,
--- Modulo03/index.js
+++ Modulo03/index.js
@@ -1,18 +1,22 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
+
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
 
-const User = mongoose.model('User');
-User.create({
-  name: 'Wesley',
-  username: 'wesleymonaro',
-  email: 'wesley@onnze.com',
-  password: '123456',
-}, () => console.log('ok'));
+app.use(bodyParser.json());
+
+app.post('/create', async (req, res) => {
+  const User = mongoose.model('User');
+
+  await User.create(req.body);
+
+  return res.send();
+});
 
 app.listen(3000);
