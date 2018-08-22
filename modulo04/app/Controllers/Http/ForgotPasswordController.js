3f1ed329ce22df535f7c8c502b1b8c409a3c6680
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-22 19:23:06 -0300

    Enviando e-mail

--- modulo04/app/Controllers/Http/ForgotPasswordController.js
+++ modulo04/app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -13,6 +14,17 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from('wesley@onnze.com', 'Wesley Monaro')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (error) {
       return response
         .status(error.status)
