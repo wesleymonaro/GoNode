15834ae0b0bfa1d24426cd71227ea64be37898cd
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-24 10:38:13 -0300

    Internacionalização

--- modulo04/app/Validators/Project.js
+++ modulo04/app/Validators/Project.js
@@ -1,10 +1,21 @@
 'use strict'
 
+const Antl = use('Antl')
+
 class Project {
+  get validateAll () {
+    return true
+  }
+
   get rules () {
     return {
-      // validation rules
+      title: 'required',
+      description: 'required'
     }
+  }
+
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
