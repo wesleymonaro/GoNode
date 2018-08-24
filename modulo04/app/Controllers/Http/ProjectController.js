4703146e0335f16293b35cc89d6e94cc02c8dcdc
Wesley Monaro <wesleymonaro@hotmail.com>
2018-08-24 10:41:23 -0300

    Paginação de projetos

--- modulo04/app/Controllers/Http/ProjectController.js
+++ modulo04/app/Controllers/Http/ProjectController.js
@@ -9,8 +9,12 @@ class ProjectController {
    * Show a list of all projects.
    * GET projects
    */
-  async index ({ request, response, view }) {
-    const projects = await Project.query().with('user').fetch()
+  async index ({ request }) {
+    const { page } = request.get()
+
+    const projects = await Project.query()
+      .with('user')
+      .paginate(page)
 
     return projects
   }
