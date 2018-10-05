5f890e6e5defbbcdab74fd41adaf5c7dace92f5d
Wesley Monaro <wesleymonaro@hotmail.com>
2018-10-04 21:31:17 -0300

    Testando registro de usuário

--- Modulo05/__tests__/integration/auth.test.js
+++ Modulo05/__tests__/integration/auth.test.js
@@ -15,6 +15,36 @@ describe('Authentication', () => {
   beforeEach(async () => {
     await User.remove();
   });
+
+  describe('Sign up', () => {
+    it('it should be able to signup', async () => {
+      const user = await factory.attrs('User');
+
+      const response = await chai
+        .request(app)
+        .post('/api/signup')
+        .send(user);
+
+      expect(response.body).to.have.property('user');
+      expect(response.body).to.have.property('token');
+    });
+
+    it('it should not be able to signup with duplicates', async () => {
+      const user = await factory.create('User');
+      const user2 = await factory.attrs('User', {
+        email: user.email,
+      });
+
+      const response = await chai
+        .request(app)
+        .post('/api/signup')
+        .send(user2);
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.have.property('error');
+    });
+  });
+
   describe('Sign in', () => {
     it('it should be able to authenticate with valid credentials', async () => {
       const user = await factory.create('User', {
